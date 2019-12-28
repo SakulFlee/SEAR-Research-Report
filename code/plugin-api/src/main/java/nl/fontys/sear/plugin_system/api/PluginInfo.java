7f2ce734ebf6b31e67fd9380f5a118e169ff5953
Lukas Weber <me@sakul6499.de>
2019-12-28 17:17:29 +0100

    [Plugin API] Added new PluginInfo annotation
    Signed-off-by: Lukas Weber <me@sakul6499.de>

---
+++ code/plugin-api/src/main/java/nl/fontys/sear/plugin_system/api/PluginInfo.java
@@ -0,0 +1,14 @@
+package nl.fontys.sear.plugin_system.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PluginInfo {
+    String name();
+
+    String version();
+}
